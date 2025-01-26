d589a1bea22e47e92a1f5b66c3edaaf4c8060e6a
NBalboa <nickojek2x@gmail.com>
2025-01-26 19:35:16 +0800

    footer on hover spin

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -16,9 +16,9 @@ const Content = ({ isSide = false, children }: ContentProps) => {
           <div className="bg-black rounded-lg p-4 relative">{children}</div>
           <a href="mailto:nickojek2x@gmail.com">
             <div>
-              <div className="relative left-6 -bottom-12 flex items-center justify-center hover:bg-black hover:text-white bg-white text-black font-bold w-[72px] h-[72px] rounded-full translate-all ease-in duration-800">
+              <div className="relative group left-6 -bottom-12 flex items-center justify-center hover:bg-black hover:text-white bg-white text-black font-bold w-[72px] h-[72px] rounded-full translate-all ease-in duration-800">
                 <p className="inline-block py-2 text-sm z-50">Hire Me</p>
-                <div className="absolute animate-spin animate-spin-slow">
+                <div className="absolute group-hover:animate-spin group-hover:animate-spin-slow">
                   <Circle />
                 </div>
               </div>
